3f3be7b0454ec52b4ee5879791c01a3fb6079a88
Marcel <nuzzo.marcel@aliceadsl.fr>
2019-11-06 14:10:32 +0100

    fixtures des 3 tables

--- src/DataFixtures/ArticleFixtures.php
+++ src/DataFixtures/ArticleFixtures.php
@@ -5,6 +5,7 @@ namespace App\DataFixtures;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 use App\Entity\Article;
+use App\Entity\Category;
 
 class ArticleFixtures extends Fixture
 {
@@ -12,14 +13,34 @@ class ArticleFixtures extends Fixture
     {
         // $product = new Product();
         // $manager->persist($product);
-        for($i=1; $i<=50; $i++) {
-           
-            $article = new Article();
-            $article ->setTitle("article n°$i")
-                     ->setContent("contenu n°$i")
-                     ->setImage("http://placehold.com/350*150")
-                     ->setCreatedAt(new \DateTime());
-            $manager->persist($article);
+        for($i = 1; $i <= 3; $i++) {
+            $categorie=new Category();
+            $category->setContent("contenu n°$i")
+                     ->setDescription("description n°$i");
+
+            $manager->persist($categorie);
+        
+            for($j=1; $j<=50; $j++) {
+            
+                $article = new Article();
+                $article ->setTitle("article n°$j")
+                        ->setContent("contenu n°$j")
+                        ->setImage("http://placehold.com/350*150")
+                        ->setCreatedAt(new \DateTime())
+                        ->setCategory($categorie);
+
+                $manager->persist($article);
+
+                for($k=1; $k<=5; $k++) {
+                    $comment = new Commentaire();
+                    $comment->setAuthor("auteur n°$k")
+                            ->setContent("contenu n°$k")
+                            ->setCreatedAt(new \DateTime())
+                            ->setArticle($article);
+
+                    $manager->persist($comment);
+                }
+            }
         }
         $manager->flush();
     }
