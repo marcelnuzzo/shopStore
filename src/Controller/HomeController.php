fd195144c39992763e0a730f3fe7c791ff756adf
Marcel <nuzzo.marcel@aliceadsl.fr>
2019-11-07 10:06:26 +0100

    commentaire author probleme

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,10 +9,42 @@ class HomeController extends AbstractController
 {
     /**
      * @Route("/home", name="home")
+     * @Route("/", name="home")
      */
     public function index()
     {
         return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+
+    /**
+    * @route("/apropos", name="apropos")
+    */
+    public function apropos()
+    {
+        return $this->render('home/apropos.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+
+    /**
+    *  @Route("/contact", name="contact")
+    */
+    public function contact()
+    {
+        return $this->render('home/contact.html.twig', [
+            'controller_name' => 'HomeController',
+            'age'=> mt_rand(1,36),
+        ]);
+    }
+
+    /**
+     *  @Route("/admin", name="admin")
+    */
+    public function admin()
+    {
+        return $this->render('home/admin.html.twig', [
             'controller_name' => 'HomeController',
         ]);
     }
