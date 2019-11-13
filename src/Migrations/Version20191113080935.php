6cdcb64bb39eda9f89ee1916b12006cc0083c03b
Marcel <nuzzo.marcel@aliceadsl.fr>
2019-11-13 09:14:19 +0100

    realText faker

--- src/Migrations/Version20191113080935.php
+++ src/Migrations/Version20191113080935.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20191112125103 extends AbstractMigration
+final class Version20191113080935 extends AbstractMigration
 {
     public function getDescription() : string
     {
