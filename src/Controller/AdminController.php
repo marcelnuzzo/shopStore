a2fbbadbe1d02afa0eff44f7729fb67e557eeaaa
Marcel <nuzzo.marcel@aliceadsl.fr>
2019-11-18 21:24:38 +0100

    admin les 4 tables

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,33 +2,84 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Article;
+use App\Entity\Category;
+use App\Entity\Commentaire;
+use App\Entity\Utilisateur;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AdminController extends AbstractController
 {
     /**
-     * @Route("/admin", name="edit_admin")
+     * @Route("/admin", name="index_admin")
      */
     public function index()
     {
-        return $this->render('admin/edit_admin.html.twig', [
+        return $this->render('admin/index_admin.html.twig', [
             'controller_name' => 'AdminController',
         ]);
     }
 
     /**
-     * @Route("/", name="categorie")
+     * @Route("/index_categorie", name="index_categorie")
+     *  @Route("/admin/{id}/editCat", name="admin_editCat")
      */
     public function categorie()
     {
         $repo = $this->getDoctrine()->getRepository(Category::class);
-
+        
         $categories = $repo->findAll();
-
-        return $this->render('admin/categorie.html.twig', [
+       
+        return $this->render('admin/index_categorie.html.twig', [
             'controller_name' => 'AdminController',
             'categories'=> $categories
         ]);
     }
+
+    /**
+     * @Route("/index_article", name="index_article")
+     *  @Route("/admin/{id}/editArt", name="admin_editArt")
+     */
+    public function article()
+    {
+        $repo = $this->getDoctrine()->getRepository(Article::class);
+        $articles = $repo->findAll();
+        
+        return $this->render('admin/index_article.html.twig', [
+            'controller_name' => 'AdminController',
+            'articles'=> $articles
+        ]);
+    }
+
+    /**
+     * @Route("/index_commentaire", name="index_commentaire")
+     *  @Route("/admin/{id}/editCom", name="admin_editCom")
+     */
+    public function commentaire()
+    {
+        $repo = $this->getDoctrine()->getRepository(Commentaire::class);
+        $commentaires = $repo->findAll();
+        
+        return $this->render('admin/index_commentaire.html.twig', [
+            'controller_name' => 'AdminController',
+            'commentaires'=> $commentaires
+        ]);
+    }
+
+    /**
+     * @Route("/index_utilisateur", name="index_utilisateur")
+     *  @Route("/admin/{id}/editUti", name="admin_editUti")
+     */
+    public function utilisateur()
+    {
+        $repo = $this->getDoctrine()->getRepository(Utilisateur::class);
+        $utilisateurs = $repo->findAll();
+        
+        return $this->render('admin/index_utilisateur.html.twig', [
+            'controller_name' => 'AdminController',
+            'utilisateurs'=> $utilisateurs
+        ]);
+    }
+
 }
