e7e78f34037bdf7bdc8e84d0655353b0a9ae7ebb
Galadan421 <36634018+Galadan421@users.noreply.github.com>
2019-12-07 12:39:59 +0100

    {User] Controlleur
    Maj du contrôleur

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,87 +2,72 @@
 
 namespace App\Controller;
 
-use App\Entity\User;
 use App\Entity\Article;
-use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Routing\Annotation\Route;
-//use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use App\Entity\User;
+use App\Form\UserType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+//use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
+/** @Route("Security") */
 class SecurityController extends AbstractController
 {
     /**
-     * @Route("/security", name="index_index")
+     * @Route("/", name="index_index")
      */
     public function index()
     {
         $repo = $this->getDoctrine()->getRepository(Article::class);
         $articles = $repo->findAll();
+
         return $this->render('security/index.html.twig', [
             'controller_name' => 'SecurityController',
-            'articles' => $articles
-        ]);
-    }
-
-   
-    /**
-    * @Route("security/formUser", name="security_formUser")
-    * 
-    */
-    public function formUser(Request $request, EntityManagerInterface $manager, UserPasswordEncoderInterface $encoder)
-    {
-        $repo = $this->getDoctrine()->getRepository(Article::class);
-        $articles = $repo->findAll();
-        
-            $user = new User();
-        
-        $form = $this->createFormBuilder($user)
-                     ->add('username')
-                     ->add('mail')
-                     ->add('login')
-                     ->add('password', PasswordType::class)
-                     ->add('confirm_password', PasswordType::class)
-                     ->getForm();
-                     
-           
-                $form->handleRequest($request);
-               
-                if($form->isSubmitted() && $form->isValid()) {
-                    $hash = $encoder->encodePassword($user, $user->getPassword());
-
-                    $user->setPassword($hash);
-                    
-                    $manager->persist($user);
-                    $manager->flush();
-                    //$this->addFlash('success', 'Votre compte à bien été enregistré.');
-                        return $this->redirectToRoute('security_login');
-                }
-
-        return $this->render('security/formUser.html.twig', [
-            'controller_name' => 'SecurityController',
             'articles' => $articles,
-            'formUser' => $form->createView()
         ]);
     }
 
+    /** @Route("/Inscription", name="security_formUser", methods={"GET", "POST"}) */
+    public function formUser(Request $request, UserPasswordEncoderInterface $encoder)
+    {   
+        $em = $this->getDoctrine()->getManager();
+        $articles = ($em->getRepository(Article::class))->findAll();
+        $user = new User($em);
+        $form = $this->get('form.factory')->create(UserType::class, $user);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid() && $request->isMethod('POST')) {
+            $em->persist($user);
+            $em->flush();
+            $this->addFlash('success', 'Votre compte à bien été enregistré.');
+
+            return $this->redirectToRoute('security_login');
+        }
+
+        $twigConfig['formUser'] = $form->createView();
+        $twigConfig['user'] = $user;
+        $twigConfig['articles'] = $articles;
+
+        return $this->render('security/formUser.html.twig', $twigConfig);
+    }
+
     /**
-     * @Route("/connexion", name="security_login")
+     * @Route("/Connexion", name="security_login")
      */
-    public function login() {
+    public function login()
+    {
         $repo = $this->getDoctrine()->getRepository(Article::class);
         $articles = $repo->findAll();
 
         return $this->render('security/login.html.twig', [
-            'articles' => $articles
+            'articles' => $articles,
         ]);
     }
 
     /**
-     * @Route("/deconnexion", name="security_logout")
+     * @Route("/Deconnexion", name="security_logout")
      */
-    public function logout() {}
-    
+    public function logout()
+    {
+    }
 }
